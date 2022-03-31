ccfef3e4ae2ebcbb0e66102c58190558debb9e95
Fulzske <61676631+Fulzske@users.noreply.github.com>
2022-03-31 13:08:46 +0000

    Your Message

--- prac3/task1.js
+++ prac3/task1.js
@@ -3,3 +3,11 @@ const d = new Date();
 function myFunction() {
     document.getElementById("current_time").innerHTML = d;
 }
+
+var counter = 0;
+
+function mouseIn() {
+    var disp = document.getElementById("mcount");
+    counter++;
+    disp.innerHTML = counter;
+}
