4ce43813e7de5d8d3ea5fd1f77cfb35bfccf24d4
Fulzske <61676631+Fulzske@users.noreply.github.com>
2022-03-31 09:16:28 +0000

    test

--- prac3/task3.js
+++ prac3/task3.js
@@ -1,8 +1,36 @@
 var counter = 0;
 
 function mouseIn() {
-    var disp = document.getElementById("mcount");
     counter++;
-    disp.innerHTML = counter;
+    document.getElementById("mcount").innerHTML = counter;
 }
+
+function postTime() {
+
+    var element = document.getElementById("posts");
+
+    var date = new Date();
+    var p1 = document.createElement("p");
+    var node1 = document.createTextNode(date);
+    p1.appendChild(node1)
+    element.appendChild(p1)
+
+    var p2 = document.createElement("p");
+    var x = document.querySelector(".post-content").value;
+    var node2 = document.createTextNode(x);
+    p2.appendChild(node2)
+    element.appendChild(p2)
+}
+
+function showMenu() {
+    document.getElementById("main").style.display = "none";
+    document.getElementById("menu").style.display = "block";
+}
+
+function showMain() {
+    document.getElementById("main").style.display = "block";
+    document.getElementById("menu").style.display = "none";
+}
+
+
 
