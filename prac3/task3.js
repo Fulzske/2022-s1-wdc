215fcd52c5de02f53f5a104d7c2dfd5bfb31572c
Fulzske <61676631+Fulzske@users.noreply.github.com>
2022-03-31 12:09:26 +0000

    test

--- prac3/task3.js
+++ prac3/task3.js
@@ -8,18 +8,22 @@ function mouseIn() {
 function postTime() {
 
     var element = document.getElementById("posts");
+    var br = document.createElement("br");
 
     var date = new Date();
-    var p1 = document.createElement("p");
+    var p1 = document.createElement("div");
     var node1 = document.createTextNode(date);
     p1.appendChild(node1)
     element.appendChild(p1)
+    element.appendChild(br);
 
-    var p2 = document.createElement("p");
+    var p2 = document.createElement("div");
     var x = document.querySelector(".post-content").value;
     var node2 = document.createTextNode(x);
-    p2.appendChild(node2)
-    element.appendChild(p2)
+    p2.appendChild(node2);
+    element.appendChild(p2);
+    element.appendChild(br);
+
 }
 
 function showMenu() {
