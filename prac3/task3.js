874b9b0b3bed98760e26d0e309085fc36fefdf13
Fulzske <61676631+Fulzske@users.noreply.github.com>
2022-03-31 09:37:14 +0000

    test

--- prac3/task3.js
+++ prac3/task3.js
@@ -10,12 +10,12 @@ function postTime() {
     var element = document.getElementById("posts");
 
     var date = new Date();
-    var p1 = document.createElement("div");
+    var p1 = document.createElement("p");
     var node1 = document.createTextNode(date);
     p1.appendChild(node1)
     element.appendChild(p1)
 
-    var p2 = document.createElement("div");
+    var p2 = document.createElement("p");
     var x = document.querySelector(".post-content").value;
     var node2 = document.createTextNode(x);
     p2.appendChild(node2)
@@ -30,6 +30,12 @@ function showMenu() {
 function showMain() {
     document.getElementById("main").style.display = "block";
     document.getElementById("menu").style.display = "none";
+}
+
+function changeBackgroundColor() {
+    var menu = document.getElementsByName("backgroundColor");
+    document.body.style.backgroundColor = menu[0].value;
+    console.log(menu[0].value);
 }
 
 
