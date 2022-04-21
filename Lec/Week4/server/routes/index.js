82039a951c8c686abd8072b4c38906ec8e83225b
Fulzske <61676631+Fulzske@users.noreply.github.com>
2022-04-21 10:36:22 +0000

    Thu, Apr 21, 2022, 10:36 AM +00:00

--- Lec/Week4/server/routes/index.js
+++ Lec/Week4/server/routes/index.js
@@ -10,5 +10,9 @@ router.get('/test', function(req, res, next) {
   res.send('this is a test');
 });
 
+router.get('/posts', function(req, res, next) {
+  res.json(post_list);
+});
+
 
 module.exports = router;
