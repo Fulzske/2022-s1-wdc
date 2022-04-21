2568f6e51958cc9b725ec7c661b9f194eabd2017
Fulzske <61676631+Fulzske@users.noreply.github.com>
2022-04-21 09:15:59 +0000

    Thu, Apr 21, 2022, 9:15 AM +00:00

--- Lec/Week4/server/routes/index.js
+++ Lec/Week4/server/routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-let post_list=[]
+let post_list=[
+  { title: "hi", desc: "test", tags: ['tag'] },
+  { title: "hi", desc: "test", tags: ['tag'] }
+];
 
 module.exports = router;
