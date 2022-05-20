8e9b688ee89cbcc4fb2f96a844484a55db82f09a
Fulzske <61676631+Fulzske@users.noreply.github.com>
2022-05-20 08:03:40 +0000

    Your Message

--- prac7/routes/users.js
+++ prac7/routes/users.js
@@ -6,20 +6,6 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-module.exports = router;
-
-router.use(function(req, res, next) {
-  if (req.method === 'POST') {
-      if (req.is('application/json') === null || req.is('application/json') === false) {
-          res.status(412).send();
-      } else {
-          next();
-      }
-  } else {
-      next();
-  }
-});
-
 var post = []
 router.post('/addpost', function(req, res, next) {
     var title = req.body.title;
@@ -37,3 +23,16 @@ router.get('/getposts', function(req, res, next) {
 });
 
 
+module.exports = router;
+
+router.use(function(req, res, next) {
+  if (req.method === 'POST') {
+      if (req.is('application/json') === null || req.is('application/json') === false) {
+          res.status(412).send();
+      } else {
+          next();
+      }
+  } else {
+      next();
+  }
+});
