0b3ac97e8d722fe249788eca92dd6c342a0d2b5d
Fulzske <61676631+Fulzske@users.noreply.github.com>
2022-05-20 04:37:58 +0000

    hey

--- prac7/routes/index.js
+++ prac7/routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/brew', function(req, res, next) {
+  var drink = req.query.drink;
+  if (drink == "tea") {
+      res.send("A delicious cup of tea.");
+  } else if (drink == "coffee") {
+      res.sendStatus(418);
+  } else {
+      res.sendStatus(400);
+  }
+});
+
 module.exports = router;
