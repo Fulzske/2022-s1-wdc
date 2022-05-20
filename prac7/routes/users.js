2fb6911d60e45533cb24272c3fdb7e0a438701e4
Fulzske <61676631+Fulzske@users.noreply.github.com>
2022-05-20 07:56:32 +0000

    Fri, May 20, 2022, 7:56 AM +00:00

--- prac7/routes/users.js
+++ prac7/routes/users.js
@@ -23,16 +23,8 @@ router.get('/getposts', function(req, res, next) {
 });
 
 
-router.use(function(req, res, next) {
-  if (req.method === "POST") {
-      if (req.is('application/json') === null || req.is('application/json') === false) {
-          res.status(412).send();
-      } else {
-          next();
-      }
-  } else {
-      next();
-  }
+router.use('/users', function(req, res, next) {
+
 });
 
 router.use(function(req, res, next) {
