625da080da7b5e953c5f58b8efe5b46c1ab1d6b4
Fulzske <61676631+Fulzske@users.noreply.github.com>
2022-05-27 08:21:54 +0000

    Your Message

--- prac8/query1-3.sql
+++ prac8/query1-3.sql
@@ -1,1 +1,6 @@
-SELECT from rental
+SELECT customer.first_name, customer.last_name, rental.rental_date
+from customer
+INNER JOIN rental ON customer.customer_id = rental.customer_id
+WHERE rental.return_date IS NULL
+ORDER BY rental.rental_date
+LIMIT 1;
