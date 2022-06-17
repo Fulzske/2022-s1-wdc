4bd1804c62e75e91404ea3c7713832e4c508b2e5
new name <61676631+Fulzske@users.noreply.github.com>
2022-06-17 03:51:00 +0000

    Fri, Jun 17, 2022, 3:51 AM +00:00

--- exam/public/javascripts/signup.js
+++ exam/public/javascripts/signup.js
@@ -5,25 +5,31 @@ function sign_up() {
         password: document.getElementById('password').value,
     };
 
-    
+    const checkUsername = document.getElementById("username");
+    const checkEmail = document.getElementById("email");
+    const checkPassword = document.getElementById("password");
 
-    var http = new XMLHttpRequest();
+    if (checkUsername.value && checkUsername.value.length > 0 && checkEmail.value && checkEmail.value.length > 0 && checkPassword.value && checkPassword.value.length > 0) {
+        var http = new XMLHttpRequest();
 
-    // Define function to run on response
-    http.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            alert("Sign up successful!");
-            window.location.href = "../index.html";
-        } else if (this.readyState == 4 && this.status == 401) {
-            alert("Email is already being used, please try another one");
-        }
-     };
+        // Define function to run on response
+        http.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                alert("Sign up successful!");
+                window.location.href = "../index.html";
+            } else if (this.readyState == 4 && this.status == 401) {
+                alert("Email is already being used, please try another one");
+            }
+        };
 
-    // Open connection to server & send the post data using a POST request
+        // Open connection to server & send the post data using a POST request
 
-    http.open("POST",  "/signup", true);
-    http.setRequestHeader("Content-type", "application/json");
-    http.send(JSON.stringify(user));
+        http.open("POST",  "/signup", true);
+        http.setRequestHeader("Content-type", "application/json");
+        http.send(JSON.stringify(user));
+    } else {
+        alert("Please enter all fields!");
+    }
 }
 
 
