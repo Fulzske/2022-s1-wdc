8282c091db501fc4c456a4161326bac471535432
new name <61676631+Fulzske@users.noreply.github.com>
2022-06-17 01:48:07 +0000

    Fri, Jun 17, 2022, 1:48 AM +00:00

--- exam/exam.sql
+++ exam/exam.sql
@@ -10,33 +10,80 @@ CREATE TABLE users (
   PRIMARY KEY (id)
 );
 
-INSERT INTO users (id, username, email, password)
-VALUES(1, "Fulei", "a1629489@student.adelaide.edu.au", '123456789');
+INSERT INTO users (username, email, password)
+VALUES("Fulei", "a1629489@student.adelaide.edu.au", '123456789');
 
-CREATE TABLE movies {
+CREATE TABLE movies (
   id int NOT NULL AUTO_INCREMENT,
   title varchar(255),
   duration int,
   genre varchar(255),
   PRIMARY KEY (id)
-};
+);
 
 INSERT INTO movies (id, title, duration, genre)
-VALUES(10, "The Hulk", "120", 'Action');
+VALUES(1, "The Hulk", 120, 'Action');
 
-CREATE TABLE theater_room {
+CREATE TABLE theater_room(
   id int NOT NULL AUTO_INCREMENT,
   name varchar(255),
   PRIMARY KEY (id)
-};
+);
 
 INSERT INTO theater_room (id, name)
-VALUES(1, "Theater #1");
+VALUES(1, "Theater room one");
 
-CREATE TABLE screening {
+CREATE TABLE screenings (
   id int NOT NULL AUTO_INCREMENT,
   movie_id int,
-  FOREIGN KEY (id) REFERENCES users(user_id) ON DELETE CASCADE,
-  FOREIGN KEY (friend_id) REFERENCES users(user_id) ON DELETE CASCADE
-};
+  theater_room_id int,
+  PRIMARY KEY (id),
+  FOREIGN KEY (movie_id) REFERENCES movies(id),
+  FOREIGN KEY (theater_room_id) REFERENCES theater_room(id)
+);
+
+INSERT INTO screenings (id, movie_id, theater_room_id)
+VALUES(1, 1, 1);
+
+CREATE TABLE seats (
+  id int NOT NULL AUTO_INCREMENT,
+  theater_room_id int,
+  seatnumber int,
+  seatletter varchar(1),
+  PRIMARY KEY (id),
+  FOREIGN KEY (theater_room_id) REFERENCES theater_room(id)
+);
+
+INSERT INTO seats (id, seatnumber, seatletter, theater_room_id)
+VALUES(1, 1, "a", 1);
+
+CREATE TABLE reserved_seats (
+  id int NOT NULL AUTO_INCREMENT,
+  seat_id int,
+  theater_room_id int,
+  screening_id int,
+  date date,
+  PRIMARY KEY (id),
+  FOREIGN KEY (seat_id) REFERENCES seats(id),
+  FOREIGN KEY (theater_room_id) REFERENCES theater_room(id),
+  FOREIGN KEY (screening_id) REFERENCES screenings(id)
+);
+
+INSERT INTO reserved_seats (id, seat_id, theater_room_id, screening_id, date)
+VALUES(1, 1, 1, 1, "2022-06-16");
+
+CREATE TABLE reservations (
+  id int NOT NULL AUTO_INCREMENT,
+  screening_id int,
+  user_id int,
+  seat_id int,
+  date date,
+  PRIMARY KEY (id),
+  FOREIGN KEY (screening_id) REFERENCES screenings(id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (seat_id) REFERENCES seats(id)
+);
+
+INSERT INTO reservations (id, screening_id, user_id, seat_id, date)
+VALUES(1, 1, 1, 1, "2022-06-16");
 
